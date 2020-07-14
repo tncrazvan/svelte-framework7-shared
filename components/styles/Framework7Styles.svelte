aa53c10346ba064862debace6b8585f1653290f2
Razvan <tangent.jotey@gmail.com>
2020-07-14 12:08:34 +0200

    Update

--- components/styles/Framework7Styles.svelte
+++ components/styles/Framework7Styles.svelte
@@ -1,6 +1,9 @@
 <style>
 	/*################# OTHER FRAMEWORK 7 STYLES ###################*/
-  :global(.sidemenu-list .list-button){
+	:global(html){
+		overscroll-behavior: none;
+	}
+  	:global(.sidemenu-list .list-button){
 		display: grid;
 		grid-template-columns: 1fr auto;
 		border-bottom: 1px solid rgba(0,0,0,0.125);
