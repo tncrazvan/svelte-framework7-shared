8e9f2dfdd20792aeb80d02fb6529afcc796a9f11
Razvan <tangent.jotey@gmail.com>
2020-11-13 12:39:40 +0100

    checkbox added

---
+++ components/overwrite/checkbox.svelte
@@ -0,0 +1,63 @@
+<script>
+    import { createEventDispatcher, onMount, afterUpdate } from 'svelte';
+    import Mixins from 'framework7-svelte/utils/mixins';
+    import Utils from 'framework7-svelte/utils/utils';
+    import restProps from 'framework7-svelte/utils/rest-props';
+  
+    const dispatch = createEventDispatcher();
+  
+    let className = undefined;
+    export { className as class };
+  
+    export let checked = undefined;
+    export let indeterminate = undefined;
+    export let name = undefined;
+    export let value = undefined;
+    export let disabled = undefined;
+    export let readonly = undefined;
+  
+    let inputEl;
+  
+    $: classes = Utils.classNames(
+      className,
+      {
+        checkbox: true,
+        disabled,
+      },
+      Mixins.colorClasses($$props),
+    );
+  
+    function onChange(event) {
+      dispatch('change', [event]);
+      if (typeof $$props.onChange === 'function') $$props.onChange(event);
+    }
+  
+    onMount(() => {
+      if (indeterminate && inputEl) {
+        inputEl.indeterminate = true;
+      }
+    });
+  
+    afterUpdate(() => {
+      if (inputEl) {
+        inputEl.indeterminate = indeterminate;
+      }
+    });
+  
+  </script>
+  
+  <label class={classes}  {...restProps($$restProps)}>
+    <input
+      bind:this={inputEl}
+      type="checkbox"
+      name={name}
+      bind:value={value}
+      disabled={disabled}
+      readonly={readonly}
+      checked={checked}
+      on:change={onChange}
+    />
+    <i class="icon-checkbox" />
+    <slot />
+  </label>
+  
